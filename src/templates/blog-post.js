fb33c11d2cd353785cde71ca54a652d946038cd3
Mat Pustizzi <mathew.pustizzi@gmail.com>
2020-12-29 08:36:57 -0500

    fix error

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -2,7 +2,7 @@
 import { jsx } from 'theme-ui'
 import { Link, graphql } from "gatsby"
 import Img from "gatsby-image"
-import { RiArrowRightLine, RiArrowLeftLine } from "react-icons/ri"
+import { RiArrowRightLine, RiArrowLeftLine, RiSendPlane2Line } from "react-icons/ri"
 
 import Layout from "../components/layout"
 import SEO from '../components/seo';
